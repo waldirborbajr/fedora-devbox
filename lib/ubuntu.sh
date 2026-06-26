e2ee7aeb01ae3f8ff933c48e18a40c90f3357be5
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-26 00:03:29 -0300

    Update ubuntu.sh

--- lib/ubuntu.sh
+++ lib/ubuntu.sh
@@ -1,3 +1,40 @@
 #!/usr/bin/env bash
-install_core_tools() { sudo apt update && sudo apt install -y git vim htop zoxide curl wget make gcc g++ podman; }
-install_pkg() { sudo apt install -y "$@"; }
+
+install_core_tools() {
+    export DEBIAN_FRONTEND=noninteractive
+
+    sudo apt-get update
+
+    local packages=(
+        git
+        vim
+        htop
+        zoxide
+        curl
+        wget
+        make
+        gcc
+        g++
+        unzip
+        tar
+        jq
+        tree
+        ca-certificates
+        bash-completion
+        software-properties-common
+        build-essential
+        pkg-config
+        podman
+    )
+
+    sudo apt-get install -y "${packages[@]}"
+}
+
+install_pkg() {
+    [[ $# -gt 0 ]] || {
+        echo "No package specified."
+        return 1
+    }
+
+    sudo apt-get install -y "$@"
+}
