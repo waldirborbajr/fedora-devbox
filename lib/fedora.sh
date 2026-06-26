fd55d3f37f3fec51d79434cde99e3409aa41acbb
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-26 00:02:36 -0300

    Update fedora.sh

--- lib/fedora.sh
+++ lib/fedora.sh
@@ -1,4 +1,33 @@
+#!/usr/bin/env bash
+
 install_core_tools() {
-  sudo dnf install -y git vim htop zoxide curl wget make gcc gcc-c++
+    local packages=(
+        git
+        vim
+        htop
+        zoxide
+        curl
+        wget
+        make
+        gcc
+        gcc-c++
+        unzip
+        tar
+        which
+        findutils
+        procps-ng
+        bash-completion
+        ca-certificates
+    )
+
+    sudo dnf install -y "${packages[@]}"
 }
-install_pkg() { sudo dnf install -y "$@"; }
+
+install_pkg() {
+    [[ $# -gt 0 ]] || {
+        echo "No package specified."
+        return 1
+    }
+
+    sudo dnf install -y "$@"
+}
