f51049e59bb572cd90baa8552408d82d2de2f477
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-26 14:34:34 +0000

    modified:   langs/infra.sh 	modified:   langs/lua.sh 	modified:   langs/python.sh

--- langs/lua.sh
+++ langs/lua.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 install_lang() {
     log_info "Instalando ambiente Lua..."
-    sudo dnf install -y lua luarocks || sudo apt install -y lua5.3 luarocks
+    # Ajuste: nomes de pacotes podem variar, mas a função install_pkg é universal
+    install_pkg lua luarocks
 }
--- langs/python.sh
+++ langs/python.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 install_lang() {
     log_info "Instalando ambiente Python..."
-    # Instala o suporte básico e ferramentas de isolamento
-    sudo dnf install -y python3-devel python3-pip || sudo apt install -y python3-dev python3-pip
-    # Instala pipx para CLI tools isoladas
+    install_pkg python3 python3-devel python3-pip
     python3 -m pip install --user pipx
     python3 -m pipx ensurepath
 }
