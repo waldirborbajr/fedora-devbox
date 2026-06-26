a567d3f66316eb8cb49ae7df2755f99fa875170a
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-26 00:04:53 -0300

    Update go.sh

--- langs/go.sh
+++ langs/go.sh
@@ -1,8 +1,26 @@
 # langs/go.sh
+
 install_lang() {
-  case "$DISTRO_TARGET" in
-    fedora) install_pkg golang ;;
-    ubuntu) install_pkg golang-go ;;
-    arch)   install_pkg go ;;
-  esac
+    case "$DISTRO_TARGET" in
+        fedora)
+            install_pkg golang
+            ;;
+        ubuntu)
+            install_pkg golang-go
+            ;;
+        arch)
+            install_pkg go
+            ;;
+        *)
+            echo "Unsupported distro: $DISTRO_TARGET"
+            return 1
+            ;;
+    esac
+
+    command -v go >/dev/null 2>&1 || {
+        echo "Go installation failed."
+        return 1
+    }
+
+    echo "Go $(go version | awk '{print $3}') installed successfully."
 }
